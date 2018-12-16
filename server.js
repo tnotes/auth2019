50efcaca9cdd5fb88c95c7a5edacd6e4a0f6d706
webdemo <kienbkav@gmail.com>
2018-12-15 20:30:06 -0800

    testing

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var request   = require('request');
 
 app.get('/',(req,res)=>{
 
-    req.pipe(request.post('http://ip-api.com/json'));
+   var pipe      = req.pipe(request('http://ip-api.com/json'));
+   var response  = [];
+
+   pipe.on('data',function(chunk) {
+      response.push(chunk);
+   });
+
+   pipe.on('end',function() {
+      var res2 = Buffer.concat(response);
+      res.send(JSON.parse(res2).query);
+
+   });
+
 });
 app.listen(process.env.PORT || 3000);
