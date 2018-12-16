deaf060c2a3f2485c00f0a25128324d317c381be
webdemo <kienbkav@gmail.com>
2018-12-15 19:49:21 -0800

    testgin

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
-    let request = require('request');
-    request.get('http://ip-api.com/json',(err,resu,body)=>{
-        let ip = JSON.parse(body).query;
-        res.send((ip))
-    })
+    var request   = require('request');
+    var pipe      = req.pipe(request.get('http://ip-api.com/json'));
+    var response  = [];
+
+    pipe.on('data',function(chunk) {
+        response.push(chunk);
+    });
+
+    pipe.on('end',function() {
+        var res2 = Buffer.concat(response);
+       res.send(JSON.parse(res2).query);
+
+    });
 });
 app.listen(process.env.PORT || 3000);
