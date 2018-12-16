ed91ec40d8915521eea635cbe61d442034e70f0a
webdemo <kienbkav@gmail.com>
2018-12-15 20:05:39 -0800

    testing

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 var request   = require('request');
 
 app.get('/',(req,res)=>{
-    request('http://ip-api.com/json').pipe(res)
+
+    req.pipe(
+        request[req.method.toLowerCase()]('http://ip-api.com/json'))
+        .pipe(res)
 });
 app.listen(process.env.PORT || 3000);
